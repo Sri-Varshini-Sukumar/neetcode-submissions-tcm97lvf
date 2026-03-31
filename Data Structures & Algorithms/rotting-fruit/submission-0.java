a2b72c41c31dfa0986a5c54e408bd34a019f47ff
Sri-Varshini-Sukumar <srivarshini1947@gmail.com>
2026-03-31 11:49:21 +0530

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int orangesRotting(int[][] arr) {
+        Queue<int[]> q = new LinkedList<>();
+
+        int count = 0;
+        int m = arr.length;
+        int n = arr[0].length;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i][j] == 1) count++;
+                else if(arr[i][j] == 2){
+                    q.offer(new int[]{i,j});
+                }
+            }
+        }
+        if(count == 0) return 0;
+        int time = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int[] currLoc = q.poll();
+                int r = currLoc[0];
+                int c = currLoc[1];
+                int[][] grid = {{r-1,c},{r,c-1},{r+1,c},{r,c+1}};
+                for(int[] nums : grid){
+                    int nr = nums[0];
+                    int nc = nums[1];
+                    if(nr < 0 || nr >= m || nc < 0 || nc >= n || 
+                    arr[nr][nc] == 0 || arr[nr][nc] == 2){
+                        continue;
+                    }
+                    q.offer(new int[]{nr,nc});
+                    arr[nr][nc] = 2;
+                    if(--count == 0) return time+1;
+
+                }
+            }
+            time++;
+        }
+
+        return (count == 0) ? time : -1;
+
+    }
+}
