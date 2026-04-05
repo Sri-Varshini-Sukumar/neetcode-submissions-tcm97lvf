5fc28ed391eb3ede06d942e7cd614b887ddd9c50
Sri-Varshini-Sukumar <srivarshini1947@gmail.com>
2026-04-05 19:44:42 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        //base case, we start with 1 and then go till the length of nums
+        HashMap<Integer,Integer> dp = new HashMap<>();
+        dp.put(0,1);
+
+        for(int num : nums){
+            HashMap<Integer,Integer> nextDp = new HashMap<>();
+            for(int sum : dp.keySet()){
+                int count = dp.get(sum);
+
+                nextDp.put(sum+num, nextDp.getOrDefault(sum+num,0)+count);
+                nextDp.put(sum-num,nextDp.getOrDefault(sum-num,0)+count);
+            }
+
+            dp = nextDp;
+        }
+
+        return dp.getOrDefault(target,0);
+    }
+}
