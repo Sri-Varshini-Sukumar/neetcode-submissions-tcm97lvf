07b0e8c1bf9dfbc5223730d0991c288e58da198b
Sri-Varshini-Sukumar <srivarshini1947@gmail.com>
2026-05-08 11:01:58 +0530

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int start = 0;
+        int end = 0;
+        String res = "";
+        
+        for(int i=0; i<n; i++){
+            start = i;
+            end = i;
+            while(start >= 0 && end < n && s.charAt(start) == s.charAt(end)){
+                start--;
+                end++;
+            }
+            String temp = s.substring(start+1,end);
+            if(temp.length() > res.length()) res = temp;
+
+            start = i;
+            end = i+1;
+            while(start >= 0 && end < n && s.charAt(start) == s.charAt(end)){
+                start--;
+                end++;
+            }
+            temp = s.substring(start+1,end);
+            if(temp.length() > res.length()) res = temp;
+        }
+        return res;
+    }
+}
